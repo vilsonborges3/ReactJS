17cb85d1fc9db22ae44596144926f0be26952fec
Vilson Borges <vilsonborges98@gmail.com>
2020-07-11 17:56:27 -0300

    Showing repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
+
+import { Loading, Owner } from './styles';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
     static propTypes = {
@@ -42,6 +46,23 @@ export default class Repository extends Component {
 
     render() {
         const { repository, issues, loading } = this.state;
-        return <h1>Repositório</h1>;
+
+        if (loading) {
+            return <Loading>Carregando</Loading>;
+        }
+        console.log(repository.name);
+        return (
+            <Container>
+                <Owner>
+                    <Link to="/">Voltar aos repositórios</Link>
+                    <img
+                        src={repository.owner.avatar_url}
+                        alt={repository.owner.login}
+                    />
+                    <h1>{repository.name}</h1>
+                    <p>{repository.description}</p>
+                </Owner>
+            </Container>
+        );
     }
 }
